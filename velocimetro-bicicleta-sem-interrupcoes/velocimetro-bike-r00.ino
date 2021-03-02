5ca9d1dcaa499832da3ab842d0e0624d39ea182c
Clovis Fritzen <clovisf@gmail.com>
2021-03-02 13:23:23 -0300

    finished (untested) speed calculation code
    finished (untested) speed calculation code. LCD 16x2 display

--- velocimetro-bicicleta-sem-interrupcoes/velocimetro-bike-r00.ino
+++ velocimetro-bicicleta-sem-interrupcoes/velocimetro-bike-r00.ino
@@ -1,20 +1,33 @@
+#include <LiquidCrystal.h>
+ 
+//Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+ 
+
 unsigned long timet;
 unsigned long previousTime;
 unsigned long previousTime2;
 boolean enterFunction = true;
 boolean enterFunction2 = true;
 
-boolean sensorPin = false;
+int sensorPin = 0;
 boolean sensorWasLow = false;
 boolean hasEnteredCounting = false;
-unsigned long lapTime;
-unsigned long initialLapTime;
-int led = 1; 
+float lapTime;
+float initialLapTime;
+float speedMs;
+float tyreCircumference= 2298;
+float speedKmh;
+#define led 1
 //-----------------------
 
 void setup() {
   // nothing happens in setup
   pinMode(led, OUTPUT);
+  pinMode(6, INPUT);
+    //Define o número de colunas e linhas do LCD
+  lcd.begin(16, 2);
+  
 }
 
 void loop() {
@@ -27,16 +40,25 @@ void loop() {
     // Write your first code below 
     //-----------------------
 
-    sensorPin= digitalRead(0); // read the speed sensor
+    sensorPin= digitalRead(6); // read the speed sensor
     
-    if (sensorPin == true && sensorWasLow == true && hasEnteredCounting == false){ // if the sensor is HIGH/Activated, the last sensor state was LOW and has not entered the loop yet
+    if (sensorPin == HIGH && sensorWasLow == true && hasEnteredCounting == false){ // if the sensor is HIGH/Activated, the last sensor state was LOW and has not entered the loop yet
       hasEnteredCounting = true; //makes the code enter here only once per 'lap'
       sensorWasLow = false; // says that the sensor was HIGH the last time 
       lapTime = millis()- initialLapTime;
+      speedMs= tyreCircumference / lapTime; // Calculate the SPEED here
+      speedKmh= speedMs * 3.6;
       initialLapTime = millis();
       
-    } else { // if the sensor is NOT ACTIVATED
-      sensorWasLow = true; // keeps track of the edge detection (from LOW to HIGH)
+    } else if (sensorPin == HIGH && hasEnteredCounting == true){ // Enter here if the sensor is still HIGH (from second passage on)
+      // Does nothing while the sensor is still HIGH
+      
+    } else if(sensorPin == LOW && hasEnteredCounting == true){ // Enter here if the sensor was HIGH and now is LOW
+      hasEnteredCounting = false;
+      
+    } else{ 
+      // Does nothing while the sensor is still LOW
+      sensorWasLow = true;
     }
   
 
@@ -58,7 +80,12 @@ void loop() {
     //-----------------------
     
 
-    //digitalWrite(led, !digitalRead(led));
+    digitalWrite(led, !digitalRead(led));
+    lcd.clear();
+  	//Posiciona o cursor na coluna 3, linha 0;
+  	lcd.setCursor(4, 0);
+  	//Envia o texto entre aspas para o LCD
+  	lcd.print(speedKmh);
 
 
 
@@ -76,7 +103,7 @@ void loop() {
     enterFunction= true;
   }
 
-  if (timet - previousTime2 < 249998){ 
+  if (timet - previousTime2 < 999999){ 
       enterFunction2= false;
   } 
   else {
